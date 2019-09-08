a628c9f57712ada67be07b1bb2948a78470d48a1
lieby weintraub <liebahinda@gmail.com>
2019-09-08 05:48:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here:
+function driversWithRevenueOver(drivers, revenue) {
+  return drivers.filter(function (drivers) {
+    return drivers.revenue > revenue
+  });
+};
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+    return driversWithRevenueOver(drivers, revenue).map(function (driver) {
+    return driver.name;
+  });
+};
