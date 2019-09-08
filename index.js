2d4770204ad8049a88534b0166f43c0eb22456f2
lieby weintraub <liebahinda@gmail.com>
2019-09-08 05:48:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ function driversWithRevenueOver(drivers, revenue) {
   return drivers.filter(function (drivers) {
     return drivers.revenue > revenue
   });
-};
+}
 
 function driverNamesWithRevenueOver(drivers, revenue) {
     return driversWithRevenueOver(drivers, revenue).map(function (driver) {
     return driver.name;
   });
-};
+}
 
 function exactMatch(drivers, attrib){
 	return drivers.filter(function (driver){
